0031a225432ef36baed6891c89021d86b5fd957b
Moreno-Osterhout, Mike <mosterhout85@gmail.com>
2021-11-01 10:13:52 -0500

    removed the hold resume section for showing the page in form for now

--- index.php
+++ index.php
@@ -131,33 +131,6 @@
 				</tr>
 			</table>
 		<?php endif; ?>
-		<?php 
-			$holdfile = "state/hold.txt";
-			$holdstatus = file_get_contents($holdfile);
-			if ($holdstatus == '1') {
-										$hold = 'green';
-									}
-			else {
-					$hold = '#686868';
-				}
-			$systemfile = "state/systemstate.txt";
-			$systemstatus = file_get_contents($systemfile);
-			if($systemstatus != '0'): ?> 
-			<table>
-				<tr>
-					<td>
-						<form action="" method="post">
-							<button style="background-color:<?php print $hold; ?>" name="hold" class="button">Hold</button>
-						</form>
-					</td>
-					<td>
-						<form action="" method="post">
-							<button name="resume" class="button">Resume</button>
-						</form>
-					</td>
-				</tr>
-			</table>
-			<?php endif; ?>
 	</div>
 
 <!--#######
